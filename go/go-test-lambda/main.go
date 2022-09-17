864ddbe3a381951048d8d7cc49683b0e817bfa10
trevor256 <256trevor@gmail.com>
2022-09-17 16:58:23 -0500

    test

--- go/go-test-lambda/main.go
+++ go/go-test-lambda/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
     "log"
-    "github.com/aws/aws-lambda-go/lambda"
 
+    "github.com/aws/aws-lambda-go/events"
+    "github.com/aws/aws-lambda-go/lambda"
 )
 
 func main() {
     lambda.Start(handler)
 }
 
-func handler (request events.APIGatewayProxyRequest) () {
+func handler (request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+    log.Println("hello")
 
+    response := events.APIGatewayProxyResponse{
+        statusCode: 200,
+    }
 
+    return response, nil
 }
+
