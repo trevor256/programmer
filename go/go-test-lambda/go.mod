56e6d538c36ae19845d9b17d04bc3448457fa658
trevor256 <256trevor@gmail.com>
2022-09-18 00:19:43 -0400

    apitesting

--- go/go-test-lambda/go.mod
+++ go/go-test-lambda/go.mod
@@ -1,3 +1,5 @@
-module golambda
+module main
 
 go 1.19
+
+require github.com/aws/aws-lambda-go v1.34.1 // indirect
--- go/go-test-lambda/main.go
+++ go/go-test-lambda/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-    "log"
-
+    "fmt"
+    "encoding/json"
     "github.com/aws/aws-lambda-go/events"
     "github.com/aws/aws-lambda-go/lambda"
 )
@@ -11,13 +11,37 @@ func main() {
     lambda.Start(handler)
 }
 
-func handler (request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    log.Println("hello")
-
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+    
+    var person Person
+    err := json.Unmarshal([]byte(request.Body), &person)
+    if err != nil {
+    	return events.APIGatewayProxyResponse{}, err
+    }
+    
+    
+    msg := fmt.Sprintf("hello %v %v", *person.FirstName, *person.LastName)
+    responseBody := ResponseBody{
+    	Message: &msg,
+    }
+    jbytes, err := json.Marshal(responseBody) 
+    if err != nil {
+    	return events.APIGatewayProxyResponse{}, err
+    }
+    
     response := events.APIGatewayProxyResponse{
-        statusCode: 200,
+        StatusCode: 200,
+        Body:       string(jbytes),
     }
 
     return response, nil
 }
 
+type Person struct {
+	FirstName *string `json:"firstName"`
+	LastName  *string `json:"lastName"`
+}
+
+type ResponseBody struct {
+	Message *string `json:"message"`
+}
